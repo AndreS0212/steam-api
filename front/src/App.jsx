f5ea0aad71501829df5a73f3bc1c44a30c04cc1a
Carlos <andre021297@gmail.com>
2023-07-24 12:18:00 -0500

    added-comments

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -4,11 +4,14 @@ import "./App.css";
 import Search from "./components/Search";
 import UserProfile from "./components/UserProfile";
 import GameList from "./components/GameList";
-import { useQuery } from "@tanstack/react-query";
+import steamIcon from "./assets/steam-icon.svg";
+import { useMutation } from "@tanstack/react-query";
 
 function App() {
   //url o usuario a buscar
   const [searchValue, setSearchValue] = useState("");
+  //Value si es true es para mostrar los mas jugados, si es false los recientes
+  //show es para mostrar el botton de cambiar
   const [switchValue, setSwitchValue] = useState({
     value: true,
     show: true,
@@ -24,10 +27,11 @@ function App() {
     status,
     savedSwitchValue,
   } = steamInfo;
-
+  //al llegar la data del fetch se setean los juegos
   useEffect(() => {
     if (userData) {
       setGames(switchValue.value ? mostPlayedGames : recentlyPlayedGames);
+      //si hay un valor guardado en la db se setea sino se usa el valor por defecto
       if (savedSwitchValue) {
         setSwitchValue(savedSwitchValue);
       }
@@ -42,33 +46,13 @@ function App() {
     }
   }, [switchValue]);
 
-  const { refetch, isLoading } = useQuery(
-    ["steam", searchValue],
-    async () => {
-      const { data } = await axios.post(
-        `http://localhost:3000/`,
-        { url: searchValue },
-        { headers: { "Content-Type": "application/json" } }
-      );
-      return data;
-    },
-    {
-      onSuccess: (data) => {
-        setSteamInfo(data);
-      },
-      onError: (error) => {
-        console.log(error);
-      },
-      enabled: false,
-    }
-  );
   const handleSearchChange = (e) => {
     setSearchValue(e.target.value);
   };
 
   const handleOnKeyDown = async (e) => {
     if (e.key === "Enter") {
-      refetch();
+      mutate();
     } else {
       return;
     }
@@ -112,29 +96,52 @@ function App() {
       return "border-gray-500";
     }
   };
+  const { mutate, isLoading, isIdle, error, data } = useMutation(
+    ["steam", searchValue],
+    async () => {
+      const { data } = await axios.post(
+        `http://localhost:3000/`,
+        { url: searchValue },
+        { headers: { "Content-Type": "application/json" } }
+      );
+      return data;
+    },
+    {
+      onSuccess: (data) => {
+        setSteamInfo(data);
+      },
+      onError: (error) => {
+        console.log(error);
+      },
+      enabled: false,
+    }
+  );
 
   return (
     <div
-      className={`max-w-[390px] ${
-        userData ? "h-[390px] bg-[#171a21] text-white rounded-xl" : ""
-      } flex flex-col max-h-[390px] mx-auto my-12 p-4 rounded-xl shadow-2xl rounded-3x bg-white`}
+      className={`${
+        isIdle || isLoading
+          ? "flex flex-col max-h-[390px]  rounded-xl shadow-2xl rounded-3x bg-white text-black"
+          : "h-[390px] bg-[#171a21]  rounded-xl text-white"
+      } mx-auto my-12 p-4 max-w-[390px] `}
       style={{
         backgroundImage:
-          userData &&
+          !isIdle &&
           `radial-gradient(farthest-side at bottom right,rgba(109, 38, 44, 0.301), transparent 500px),
                    radial-gradient(farthest-corner at bottom left, rgba(50, 255, 193, 0.103), transparent 600px)`,
-        backgroundColor: steamInfo.status && "rgba(34, 35, 48, 0.93)",
-        backgroundRepeat: steamInfo.status ? "no-repeat" : "initial",
+        backgroundColor: !isIdle && "rgba(34, 35, 48, 0.93)",
+        backgroundRepeat: !isIdle ? "no-repeat" : "initial",
         backgroundPosition: "center",
       }}
     >
-      {!userData ? (
-        <Search
-          searchValue={searchValue}
-          handleSearchChange={handleSearchChange}
-          handleOnKeyDown={handleOnKeyDown}
-        />
-      ) : (
+      {isLoading ? (
+        <p className="text-white">Loading...</p>
+      ) : error ? (
+        <div className="flex items-center h-full">
+          <img src={steamIcon} alt="" className="h-9 mb-1" />
+          <p className="text-2xl">Usuario o link de steam inválido. </p>
+        </div>
+      ) : data ? (
         <>
           <UserProfile
             userData={userData}
@@ -142,11 +149,20 @@ function App() {
             setSwitchValue={setSwitchValue}
             switchValue={switchValue}
             colorStatus={colorStatus}
+            hasGames={games?.length > 0}
           />
-          {games && (
-            <GameList filterGames={games} switchValue={switchValue?.value} />
-          )}
+          <GameList
+            filterGames={games}
+            switchValue={switchValue?.value}
+            hasGames={games?.length > 0}
+          />
         </>
+      ) : (
+        <Search
+          searchValue={searchValue}
+          handleSearchChange={handleSearchChange}
+          handleOnKeyDown={handleOnKeyDown}
+        />
       )}
     </div>
   );
